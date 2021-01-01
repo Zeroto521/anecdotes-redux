d11b396f9517ecd5b417f33466ad4bcc0e7756f0
Zero <zeroto521@gmail.com>
2021-01-01 12:50:15 +0800

    :tada: finish 6.5

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -20,9 +20,11 @@ const initialState = anecdotesAtStart.map(asObject)
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'VOTE':
-      return state.map(as =>
+      const newState = state.map(as =>
         as.id !== action.data.id ? as : { ...as, 'votes': as.votes + 1 }
       )
+      newState.sort((a, b) => b.votes - a.votes)
+      return newState
     case 'NEW':
       return [...state, action.data]
     default:
